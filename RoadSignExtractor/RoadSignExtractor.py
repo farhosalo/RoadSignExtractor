d1964329142217e6e5f6a0a89f10fa74b78d0691
Salo <info@salo.com.de>
2024-10-01 22:58:29 +0200

    Download the GroundingDINO configuration file alongside the GroundingDINO model

--- RoadSignExtractor/RoadSignExtractor.py
+++ RoadSignExtractor/RoadSignExtractor.py
@@ -7,7 +7,8 @@ import re
 
 
 class RoadSignExtractor:
-    __GDinoUrl = "https://github.com/IDEA-Research/GroundingDINO/releases/download/v0.1.0-alpha/groundingdino_swint_ogc.pth"
+    __GDinoModelUrl = "https://github.com/IDEA-Research/GroundingDINO/releases/download/v0.1.0-alpha/groundingdino_swint_ogc.pth"
+    __GDinoConfigUrl = "https://raw.githubusercontent.com/IDEA-Research/GroundingDINO/refs/tags/v0.1.0-alpha2/groundingdino/config/GroundingDINO_SwinT_OGC.py"
     __WeightDir = os.path.join(os.getcwd(), "Weights")
     __Model = None
     __MinimumSignSize = (44, 44)
@@ -42,17 +43,20 @@ class RoadSignExtractor:
         return -1
 
     def __DownloadModel(self):
-        fileName = self.__GDinoUrl.rsplit("/", 1)[-1]
-        completeFileName = os.path.join(self.__WeightDir, fileName)
+        for url in [self.__GDinoModelUrl, self.__GDinoConfigUrl]:
+            fileName = url.rsplit("/", 1)[-1]
+            completeFileName = os.path.join(self.__WeightDir, fileName)
 
-        if not os.path.exists(completeFileName):
-            request.urlretrieve(self.__GDinoUrl, completeFileName)
+            if not os.path.exists(completeFileName):
+                request.urlretrieve(url, completeFileName)
 
     def __LoadModel(self):
         if self.__Model == None:
-            configPath = "config/GroundingDINO_SwinT_OGC.py"
+            configPath = os.path.join(
+                self.__WeightDir, self.__GDinoConfigUrl.rsplit("/", 1)[-1]
+            )
             checkPoint = os.path.join(
-                self.__WeightDir, self.__GDinoUrl.rsplit("/", 1)[-1]
+                self.__WeightDir, self.__GDinoModelUrl.rsplit("/", 1)[-1]
             )
             self.__Model = Model(
                 model_config_path=configPath,
